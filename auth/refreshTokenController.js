b5f56b64b6719eec21c49e6f5e47c4e7d8259ff9
Hamza Ashraf <hamza.ashraf@live.co.uk>
2025-06-11 21:34:04 +0100

    set up refresh token controller

---
+++ auth/refreshTokenController.js
@@ -0,0 +1,39 @@
+// Function to allow users to request a new access token using a valid refresh token
+const jwt = require('jsonwebtoken');
+const db = require('../prisma/queries')
+require('dotenv').config();
+
+const JWT_REFRESH_SECRET_KEY = process.env.JWT_REFRESH_SECRET_KEY
+const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY;
+
+async function refreshToken(req, res) {
+  const refreshToken = req.cookies.refreshToken;              // Retrieve the refresh token from the user's cookies
+  if (!token) return res.status(401).json({ message: 'Refresh Token is missing' });  // Unauthorized
+
+
+  try {
+    // Evaluate the JWT 
+    const payload = jwt.verify(refreshToken, JWT_REFRESH_SECRET_KEY);  // Verifying the refresh token to see if its still valid and hasn't been tampered with. If valid, it returns the decoded pauload including user id & claims.
+    
+
+    // Verify to see if refresh token is valid
+    const storedToken = await db.getRefreshTokenByUserId(payload.id);
+    if (storedToken != refreshToken) {
+      res.status(403).json({ message: 'Invalid refresh token' })
+    }
+
+    
+    // Create a new access token with user ID and role from the verified payload
+    const newAccessToken = jwt.sign({ id: payload.id, role: payload,role}, JWT_SECRET_KEY, {
+      expiresIn: '30m'
+    });
+
+    res.json({ accessToken: newAccessToken})    
+    // Send the new access token back to the client in the response
+  } catch (error) {
+    res.status(403).json({ message: 'Invalid or expired refresh token' });     // If verification fails or an error occurs, respond with 403 Forbidden
+  }
+}
+
+
+module.exports = refreshToken;
