cb5c498747fe82b75624c2ef2edd9a7e55d79412
Hamza Ashraf <hamza.ashraf@live.co.uk>
2025-06-11 19:49:23 +0100

    controller functions for comment routes

---
+++ controllers/commentsController.js
@@ -0,0 +1,110 @@
+const db = require('../prisma/queries');
+
+
+
+// GET /posts/:postId/comments/:commentId    - a single comment of a post
+async function getSingleCommentOfPostController(req, res) {
+  try {
+    const postId = parseInt(req.params.postId, 10);
+    const commentId = parseInt(req.params.commentId, 10)
+
+    const comment = await db.getSingleCommentOfPost(commentId, postId)
+
+    if (!comment) {
+      return res.status(404).json({ error: 'Comment not found' })
+    }
+
+    res.json(comment)
+  } catch (error) {
+    res.status(500).json({ error:'Failed to retrieve comment' })
+  }
+}
+
+
+
+// POST posts/:postId/comments/comment   - Create a new comment on a post 
+async function createCommentController(req, res) {
+  try {
+    const postId = parseInt(req.params.postId, 10);
+    const userId = req.user?.id;
+    const { content } = req.body;
+
+    const comment = await db.createComment(postId, userId, content)
+    
+    res.status(201).json(comment)
+
+  } catch (error) {
+    res.status(500).json({ error:'Internal Server Error' })
+  }
+  
+}
+
+
+// PUT posts/:postId/comments/:commentId  - Update comment using post Id and comment Id
+async function updateCommentController(req, res) {
+  try {
+    const userId = req.user?.id;
+    const postId = parseInt(req.params.postId, 10);
+    const commentId = parseInt(req.params.commentId, 10);
+    const { content } = req.body;
+    
+    
+    const comment = await db.getSingleCommentOfPost(commentId);
+
+    if (!comment) {
+      return res.status(404).json({ error: 'Comment not found' })
+    }
+
+     if (comment.postId !== postId) {
+      return res.status(404).json({ error: 'Comment does not belong to this post' });
+    }
+
+    if (comment.userId !== userId) {
+      return res.status(404).json({ error: 'You are not authorized to edit this comment' });
+    }
+
+    const updatedComment = await db.updateComment(commentId, content);
+    
+    res.json(updatedComment)
+    
+
+  } catch (error) {
+    res.status(500).json({ error: 'Failed to update commment' });
+  }
+}
+
+
+
+// DELETE posts/:postId/comments/:commentId   - Delete comment using post id and comment id
+async function deleteCommentController(req, res) {
+  try {
+    const commentId = parseInt(req.params.commentId);
+    const userId = req.user?.id;
+
+    const comment = await db.getSingleCommentOfPost(commentId);
+
+    if (!comment) {
+      return res.status(404).json({ error: 'Comment not found' })
+    }
+
+
+    if (comment.userId !== userId) {
+      return res.status(403).json({ error: 'You are not authorized to delete this comment' })
+    }
+
+    await db.deleteComment(comment)
+
+    res.status(204).json({ message: 'Comment deleted', commentId });
+    
+  } catch (error) {
+    res.status(500).json({ error: 'Failed to delete comment' })
+  }
+}
+
+
+module.exports = {
+  getSingleCommentOfPostController,
+  createCommentController,
+  updateCommentController,
+  deleteCommentController
+}
