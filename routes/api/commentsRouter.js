e6c4cdb92f26a603c95c9a08a3a41b5055764168
Hamza Ashraf <hamza.ashraf@live.co.uk>
2025-06-24 19:08:01 +0100

    added mergeParams to postId param for main entry file

--- routes/api/commentsRouter.js
+++ routes/api/commentsRouter.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const { getSingleCommentOfPostController, createCommentController, updateCommentController, deleteCommentController  } = require('../../controllers/commentsController')
-const router = express.Router()
+const router = express.Router({ mergeParams: true })
 
 // GET a specific comment on a post
 router.get('/:commentId', getSingleCommentOfPostController);
@@ -22,3 +22,23 @@ router.delete('/:commentId', deleteCommentController);
 
 
 module.exports = router;
+
+
+
+
+/*
+mergeParams: true
+
+By defauly express doesn't pass the :postId paramter from the parent route in app.js to this child router 
+which is why the / route wasn't grabbing the postId from url.
+
+
+mergeParams: true tells express to carry over any params from the parent route when commentsRouter is mounted.
+
+
+
+mergeParams: true is required to access parent route parameters like :postId inside a nested router.
+
+Without it, req.params in commentsRouter will only include parameters defined within the router itself, not the parent path where it's mounted.
+
+*/
