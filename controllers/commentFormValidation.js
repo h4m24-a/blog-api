ada598b3aaff0968284f15f92b5c2b8483579304
Hamza Ashraf <hamza.ashraf@live.co.uk>
2025-08-14 03:25:54 +0100

    form validation for update comment route

--- controllers/commentFormValidation.js
+++ controllers/commentFormValidation.js
@@ -3,11 +3,21 @@ const { body } = require('express-validator');  // form validation
 const validateComment = [
   body('content')
     .trim()
-    .isLength({ min: 1, max: 200 })
+    .isLength({ min: 3, max: 200 })
+    .withMessage('Comment must be between 1 and 50 characters long.')
+    .escape(), // Sanitization: Escape special characters
+];
+
+
+const validateUpdateComment = [
+  body('updatedContent')
+    .trim()
+    .isLength({ min: 3, max: 200 })
     .withMessage('Comment must be between 1 and 50 characters long.')
     .escape(), // Sanitization: Escape special characters
 ];
 
 
 
-module.exports = { validateComment };
+
+module.exports = { validateComment, validateUpdateComment  };
